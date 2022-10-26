5234611f8b938d1bdecae5ba67ed45db40debdaa
nonbinary00 <agarkovaalysh12@gmail.com>
2022-10-26 02:28:31 +0200

    task
    Made a request for a backend,
    Аdded libraries
    Event listener for input
    Using debounce to prevent chatty event

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,20 +1,28 @@
 const BASIC_URL = `https://restcountries.com/v3.1/name`;
 const FILTER_RESPONSE = `fields=name,capital,population,flags,languages`;
 
+// const refp = fetch(
+//          `https://restcountries.com/v3.1/name/${BASIC_URL}?${FILTER_RESPONSE}`
+//         );
+// refp.then(response => console.log(response));
+
+
+
+
 export const fetchCountries = name => {
     return fetch(
       `https://restcountries.com/v3.1/name/${BASIC_URL}?${FILTER_RESPONSE}`
     )
-        .then(response => {
-        if (!response.ok) {
-          if (response.status === 404) {
-            return [];
-          }
-          throw new Error(response.status);
-        }
-        return response.json();
-      })
-      .catch(error => {
-        console.error(error);
+        .then(response => console.log(response),{
+    //     if (!response.ok) {
+    //       if (response.status === 404) {
+    //         return [];
+    //       }
+    //       throw new Error(response.status);
+    //     }
+    //     return response.json();
+    //   })
+    //   .catch(error => {
+    //     console.error(error);
       });
   };
--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 import './css/styles.css';
+import { fetchCountries } from './fetchCountries';
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
 
+
+const input = document.querySelector('#search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
 const DEBOUNCE_DELAY = 300;
+
+// Слушатель событий для ввода
+//Используем debounce для предотвращения болтливого события
+
+input.addEventListener(
+    'input',
+    debounce(e => {
+        const trimmedValue = input.value.trim();
+           cleanHtml();   
+      if (trimmedValue !== '') {
+          fetchCountries(trimmedValue).then(foundData => {      
+  
+          if (foundData.length > 10) {
+            Notiflix.Notify.info(
+              'Too many matches found. Please enter a more specific name.'
+            );
+          } else if (foundData.length === 0) {
+            Notiflix.Notify.failure('Oops, there is no country with that name');
+          } else if (foundData.length >= 2 && foundData.length <= 10) {
+           
+            renderCountryList(foundData);
+          } else if (foundData.length === 1) {
+      
+            renderOneCountry(foundData);
+          }
+        });
+      }
+    }, DEBOUNCE_DELAY)
+  );
